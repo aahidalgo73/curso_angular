068ad8e5dbdb15e1516c164c9daae001d0f62956
aahidalgo <aahidalgo73@hotmail.com>
2025-07-03 18:28:28 -0500

    actualizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule} from '@angular/common';
+import { RouterOutlet} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [FormsModule, CommonModule, RouterOutlet],
+  templateUrl:'./app.html',
+  styleUrl:'./app.scss',
+  standalone:true,
 })
-export class App {
-  protected title = 'todo-list';
+
+export class App{
+  title="PRODUCTOS";
+  task="";
+  tasks:{id: number, name:string}[]=[];
+  
+  addTask(){
+    if(this.task.trim()==="") return;
+    this.tasks.push({id: this.tasks.length +1, name: this.task});
+    this.task="";
+  }
+
+  removeTask(id: number){
+    this.tasks=this.tasks.filter(task => task.id !==id);
+  }
+  
 }
